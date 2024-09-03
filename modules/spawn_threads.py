95c6eff21a7217f9853453bedd886ac84563290a
Michael Caballero <mcaballero.dev@gmail.com>
2024-09-03 11:40:38 -0700

    Rename thread_functions.py

--- modules/spawn_threads.py
+++ modules/spawn_threads.py
@@ -1,68 +1,8 @@
-import configparser
 import threading
 import time
 from typing import List, Tuple
-from timestamp import ts_print
-from network_functions import *
-
-# pull configuration from '../config.ini'
-config = configparser.ConfigParser()
-config.read('../config.ini')
-
-
-def icmp_test(server_name: str, server: str, ttl: int, timeout: int,
-              sequence_number: int, interval: int,
-              stop_event: threading.Event):
-    while not stop_event.is_set():
-        time_limit = interval
-        ping_addr, ping_time = ping(server, ttl, timeout, sequence_number)
-        output = f"[{server_name}]".ljust(longest_name) + " [ICMP] "
-        if ping_time is None:
-            output += "[FAIL]".ljust(6) + \
-                f"[{ping_addr[0]}, 'No reponse received']"
-        else:
-            output += "[OK]".ljust(6) + f"[{ping_addr[0]}, {ping_time:.2f}ms]"
-        ts_print(output)
-        while time_limit > 0:
-            time.sleep(0.1)
-            time_limit -= 0.1
-            if stop_event.is_set():
-                break
-
-
-def http_test(server_name: str, server: str, interval: int,
-              stop_event: threading.Event):
-    pass
-
-
-def https_test(server_name: str, server: str, interval: int,
-               stop_event: threading.Event):
-    pass
-
-
-def dns_test(server_name: str, server: str, dns_records: str,
-             interval: int, stop_event: threading.Event):
-    pass
-
-
-def ntp_test(server_name: str, server: str, interval: int,
-             stop_event: threading.Event):
-    pass
-
-
-def tcp_test(server_name: str, server: str, port: int, interval: int,
-             stop_event: threading.Event):
-    pass
-
-
-def udp_test(server_name: str, server: str, port: int, interval: int,
-             stop_event: threading.Event):
-    pass
-
-
-def echo_test(server_name: str, server: str, type: str, port: int,
-              stop_event: threading.Event):
-    pass
+from globals import config
+import tests
 
 
 def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
@@ -94,9 +34,9 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             sequence_number = config.getint(section, 'icmp_sequence_number')
             # create thread
             t = threading.Thread(
-                target=icmp_test, args=(section, server, ttl, timeout,
-                                        sequence_number, interval,
-                                        stop_event,))
+                target=tests.icmp_test,
+                args=(section, server, ttl, timeout, sequence_number, interval,
+                      stop_event,))
             # append thread to list
             threads.append(t)
 
@@ -105,8 +45,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             print(f"HTTP test initialized for {section}")
             interval = config.getint(section, 'http_interval')
             t = threading.Thread(
-                target=http_test, args=(section, server, interval,
-                                        stop_event,))
+                target=tests.http_test,
+                args=(section, server, interval, stop_event,))
             threads.append(t)
 
         # HTTPS test
@@ -114,8 +54,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             print(f"HTTPS test initialized for {section}")
             interval = config.getint(section, 'https_interval')
             t = threading.Thread(
-                target=https_test, args=(section, server, interval,
-                                         stop_event,))
+                target=tests.https_test,
+                args=(section, server, interval, stop_event,))
             threads.append(t)
 
         # DNS test
@@ -124,8 +64,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             interval = config.getint(section, 'dns_interval')
             dns_records = config.get(section, 'dns_records')
             t = threading.Thread(
-                target=dns_test, args=(section, server, dns_records, interval,
-                                       stop_event,))
+                target=tests.dns_test,
+                args=(section, server, dns_records, interval, stop_event,))
             threads.append(t)
 
         # NTP test
@@ -133,7 +73,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             print(f"NTP test initialized for {section}")
             interval = config.getint(section, 'ntp_interval')
             t = threading.Thread(
-                target=ntp_test, args=(section, server, interval, stop_event,))
+                target=tests.ntp_test,
+                args=(section, server, interval, stop_event,))
             threads.append(t)
 
         # TCP test
@@ -142,8 +83,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             interval = config.getint(section, 'tcp_interval')
             port = config.getint(section, 'tcp_port')
             t = threading.Thread(
-                target=tcp_test, args=(section, server, port, interval,
-                                       stop_event,))
+                target=tests.tcp_test,
+                args=(section, server, port, interval, stop_event,))
             threads.append(t)
 
         # UDP test
@@ -152,8 +93,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             interval = config.getint(section, 'udp_interval')
             port = config.getint(section, 'udp_port')
             t = threading.Thread(
-                target=udp_test, args=(section, server, port, interval,
-                                       stop_event,))
+                target=tests.udp_test,
+                args=(section, server, port, interval, stop_event,))
             threads.append(t)
 
         # Echo test
@@ -162,8 +103,8 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
             echo_type = config.get(section, 'echo_type')
             port = config.getint(section, 'echo_port')
             t = threading.Thread(
-                target=echo_test, args=(section, server, echo_type, port,
-                                        stop_event,))
+                target=tests.echo_test,
+                args=(section, server, echo_type, port, stop_event,))
             threads.append(t)
 
         # Traceroute Test
@@ -175,14 +116,6 @@ def generate_threads() -> Tuple[List[threading.Thread], threading.Event]:
 
 
 if __name__ == "__main__":
-    global longest_name
-
-    longest_name: int = 0
-    for section in config.sections():
-        if len(section) > longest_name:
-            longest_name = len(section)
-    longest_name += 2
-
     threads, stop_event = generate_threads()
 
     for thread in threads:
